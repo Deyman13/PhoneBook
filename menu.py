0480899113190e5f4a24ab4a87ca239f7b768709
Daniil <desmond229@mail.ru>
2022-12-23 15:18:12 +0300

    Добавлена поддержка английского языка в меню

--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
-from function import init_dict, print_book, write_new_contact_list, write_new_contact_str, replace_contact_list, replace_contact_str, delete_contact_str, delete_contact_list, clear
+from function import init_dict, print_book, write_new_contact_list, write_new_contact_str, replace_contact_list 
+from function import replace_contact_str, delete_contact_str, delete_contact_list, clear, translate_str, translate_list
 from inputreplasechange import search
 
 
-def print_hint():
+def print_hint_ru():
     print('''\n                  
         |====================================================================|
         |           ===>> КОМАНДЫ ДЛЯ РАБОТЫ СО СПРАВОЧНИКОМ <<===           |
@@ -23,57 +24,135 @@ def print_hint():
         ||                                                                  ||
         ||-*-*-*-*--------------------------------------------------*-*-*-*-||''')
 
+def print_hint_eng():
+    print('''\n                  
+        |====================================================================|
+        |       ===>> COMMANDS FOR WORKING WITH THE DIRECTORY <<===          |
+        |====================================================================|
+
+        ||-*-*-*-*--------------------------------------------------*-*-*-*-||
+        ||                                                                  ||
+        ||     Exiting the program: 0 or Enter                             ||
+        ||     View all entries of both directories:  - 1                  ||
+        ||     Adding a new entry to the string directory: - 2             ||
+        ||     Adding a new entry to the list directory: - 3               ||
+        ||     Search for the necessary contact by parameters - 4          ||
+        ||     Changing an entry from a string directory by data: - 5      ||
+        ||     Changing an entry from the list directory by data: - 6      ||
+        ||     Deleting an entry from a string data reference: - 7         ||
+        ||     Deleting an entry from a list data reference: - 8           ||
+        ||     Clearing the terminal - 9                                   ||
+        ||                                                                  ||
+        ||-*-*-*-*--------------------------------------------------*-*-*-*-||''')
+
 
 def menu():
-    print()
-    print("ВНИМАНИЕ!!!")
-    print("ПОЖАЛУЙСТА, РАСКРОЙТЕ ТЕРМИНАЛ ПОЛНОСТЬЮ ДЛЯ УДОБНОГО ПОЛЬЗОВАНИЯ НАШЕЙ ПРОГРАММОЙ!!!")
-    flag = True
-    while flag:
+    language = input("Выберите язык / Select a language: RU or ENG: ")
+    if language.lower() == "ru":
         print()
-        ans = input("Вы раскрыли терминал? Введите ДА или НЕТ: ")
-        print()
-        if ans.lower() == "да":
-            flag = False
-            print("                 || ===---===---===---===---===---===---===---=== ||")
-            print("                 |-|-|       ПРИЯТНОГО ПОЛЬЗОВАНИЯ!!! :)       |-|-|")
-            print("                 || ===---===---===---===---===---===---===---=== ||")
-            book_all = {}
-            init_dict(book_all, "str")
-            init_dict(book_all, "lst")
-            dict_command = {'1': print_book, '2': write_new_contact_str,
-                            '3': write_new_contact_list, '4': search, '5': replace_contact_str,
-                            '6': replace_contact_list, '7': delete_contact_str, '8': delete_contact_list}
-            print_hint()
-            while True:
+        print("ВНИМАНИЕ!!!")
+        print("ПОЖАЛУЙСТА, РАСКРОЙТЕ ТЕРМИНАЛ ПОЛНОСТЬЮ ДЛЯ УДОБНОГО ПОЛЬЗОВАНИЯ НАШЕЙ ПРОГРАММОЙ!!!")
+        flag = True
+        while flag:
+            print()
+            ans = input("Вы раскрыли терминал? Введите ДА или НЕТ: ")
+            print()
+            if ans.lower() == "да":
+                flag = False
+                print("                 || ===---===---===---===---===---===---===---=== ||")
+                print("                 |-|-|       ПРИЯТНОГО ПОЛЬЗОВАНИЯ!!! :)       |-|-|")
+                print("                 || ===---===---===---===---===---===---===---=== ||")
+                book_all = {}
+                init_dict(book_all, "str")
+                init_dict(book_all, "lst")
+                dict_command = {'1': print_book, '2': write_new_contact_str,
+                                '3': write_new_contact_list, '4': search, '5': replace_contact_str,
+                                '6': replace_contact_list, '7': delete_contact_str, '8': delete_contact_list}
+                print_hint_ru()
+                while True:
+                    print()
+                    print("===>> Введите help, чтобы вывести список команд, если вы забыли их! <<===")
+                    print("===>> Чтобы очистить терминал нажмите 9 <<===")
+                    command = input('Команда: > ')
+                    if command in dict_command:
+                        dict_command[command](book_all)
+                    elif command == "0" or command == '':
+                        print("| ---------------------------------------------------------- |")
+                        print("|     ВЫ ВЫШЛИ ИЗ ПРОГРАММЫ, НАДЕЕМСЯ ВАМ ПОНРАВИЛОСЬ!)      |")
+                        print("|                                                            |")
+                        print("|    Работа выполнена Шкляевым Даниилом, Лукашенок Борисом   |")
+                        print("|                                                            |")
+                        print("|    Даниил - Backend, Frontend                              |")
+                        print("|    Борис - Backend, ProjectManager, MainLogic              |")
+                        print("|    Все права защищены © Dec 22, 2022 DanBOR, Inc           |")
+                        print("| ---------------------------------------------------------- |")
+                        break
+                    elif command == "9":
+                        clear()
+                    elif command.lower() == "help":
+                        print_hint_ru()
+                    else:
+                        print('Команда не распознана, попробуйте снова')   
+            elif ans.lower() == "нет":
                 print()
-                print("===>> Введите help, чтобы вывести список команд, если вы забыли их! <<===")
-                print("===>> Чтобы очистить терминал нажмите 9 <<===")
-                command = input('Команда: > ')
-                if command in dict_command:
-                    dict_command[command](book_all)
-                elif command == "0" or command == '':
-                    print("| ---------------------------------------------------------- |")
-                    print("|     ВЫ ВЫШЛИ ИЗ ПРОГРАММЫ, НАДЕЕМСЯ ВАМ ПОНРАВИЛОСЬ!)      |")
-                    print("|                                                            |")
-                    print("|    Работа выполнена Шкляевым Даниилом, Лукашенок Борисом   |")
-                    print("|                                                            |")
-                    print("|    Даниил - Backend, Frontend                              |")
-                    print("|    Борис - Backend, ProjectManager, MainLogic              |")
-                    print("|    Все права защищены © Dec 22, 2022 DanBOR, Inc           |")
-                    print("| ---------------------------------------------------------- |")
-                    break
-                elif command == "9":
-                    clear()
-                elif command.lower() == "help":
-                    print_hint()
-                else:
-                    print('Команда не распознана, попробуйте снова')   
-        elif ans.lower() == "нет":
+                print("Тогда откройте его. Это важно!")
+                print()
+            else:
+                print("Некорректный ввод! Программа будет завершена!")
+                print()
+                break
+    elif language.lower() == "eng":
+        print()
+        print("ATTENTION!!!")
+        print("PLEASE OPEN THE TERMINAL COMPLETELY FOR CONVENIENT USE OF OUR PROGRAM!!!")
+        flag = True
+        while flag:
             print()
-            print("Тогда откройте его. Это важно!")
+            ans = input("Have you opened the terminal? Enter YES or NO: ")
             print()
-        else:
-            print("Некорректный ввод! Программа будет завершена!")
-            print()
-            break
+            if ans.lower() == "yes":
+                flag = False
+                print("                 || ===---===---===---===---===---===---===---=== ||")
+                print("                 |-|-|            ENJOY YOUR USE!!! :)         |-|-|")
+                print("                 || ===---===---===---===---===---===---===---=== ||")
+                book_all = {}
+                init_dict(book_all, "str")
+                init_dict(book_all, "lst")
+                dict_command = {'1': print_book, '2': write_new_contact_str,
+                                '3': write_new_contact_list, '4': search, '5': replace_contact_str,
+                                '6': replace_contact_list, '7': delete_contact_str, '8': delete_contact_list}
+                print_hint_eng()
+                while True:
+                    print()
+                    print("===>> Type help to list commands if you forgot them! <<===")
+                    print("===>> To clear the terminal, press 9 <<===")
+                    command = input('Command: > ')
+                    if command in dict_command:
+                        dict_command[command](book_all)
+                    elif command == "0" or command == '':
+                        print("| ---------------------------------------------------------- |")
+                        print("|     YOU HAVE LEFT THE PROGRAM, WE HOPE YOU ENJOYED IT!)    |")
+                        print("|                                                            |")
+                        print("|    The work was done by Daniel Shklyaev, Boris Lukashenok  |")
+                        print("|                                                            |")
+                        print("|    Daniel - Backend, Frontend                              |")
+                        print("|    Boris - Backend, ProjectManager, MainLogic              |")
+                        print("|    All rights reserved © Dec 22, 2022 DanBOR, Inc          |")
+                        print("| ---------------------------------------------------------- |")
+                        break
+                    elif command == "9":
+                        clear()
+                    elif command.lower() == "help":
+                        print_hint_eng()
+                    else:
+                        print('The command is not recognized, try again')   
+            elif ans.lower() == "no":
+                print()
+                print("Then open it. It is important!")
+                print()
+            else:
+                print("Incorrect input! The program will be completed!")
+                print()
+                break
+
+
